ef9057fc5a7a58fe320439b26f6584319421337d
Santiago Lovón García <santiago.lg.1994@gmail.com>
2014-04-13 14:43:08 -0500

    Update cachematrix.R
    Some comments and the function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This functions enables to optimize the calculation of matrix inversion by saving calculated values on the cache
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix converts a matrix into a list containing functions to get and set the values of the matrix and the inverse
+## Input: A invertible matrix
+## Output: A list with the get, set, getinverse and  setinverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
